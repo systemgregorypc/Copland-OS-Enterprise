9e882e1d6717ebe7a34f099c8e926d236e8187ee
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2022-04-20 20:51:30 -0400

    Update myfirst.asm

--- iso/myfirst.asm
+++ iso/myfirst.asm
@@ -16,7 +16,7 @@ start:
 	jmp $			; Jump here - infinite loop!
 
 
-	text_string db 'This is my cool new OS!', 0
+	text_string db 'This is my cool Copland-OS-Enterprise!', 0
 
 
 print_string:			; Routine: output string in SI to screen
